39bfeb3ae04313510c33c13e7c6765f5c1d43d1f
mystore <hamo6693@gmail.com>
2022-08-20 23:12:21 +0300

    commit 2020/8/20

--- src/index.js
+++ src/index.js
@@ -5,23 +5,14 @@ import "jquery/dist/jquery.min";
 import "popper.js/dist/popper.min.js";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '@fortawesome/fontawesome-free/js/all.min.js';
-
+import 'animate.css/animate.css';
 
 var date = new Date();
 var year = date.getFullYear();
 document.getElementById("date").innerHTML = year;
 
-var $password = document.getElementById("password");
-var $mybtn = document.getElementById("mybtn");
 
-$mybtn.onclick = function () {
-    if(this.textContent='show password') {
-        $password.setAttribute("type","text");
-        this.textContent="hide password";
-    } else {
-        $password.setAttribute("type","password");
-    }
-    this.textContent = "show password";
-}
+
+
 
 
